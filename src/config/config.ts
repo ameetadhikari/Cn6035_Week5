1a7429086d07952ad0916bf6ff330478b8f5736b
ameetadhikari <75064957+ameetadhikari@users.noreply.github.com>
2024-03-14 14:57:26 +0000

    moved ts and delete js

---
+++ src/config/config.ts
@@ -0,0 +1,14 @@
+import algosdk from "algosdk";
+const algodToken = "a".repeat(64);
+const server: string = "http://localhost";
+const port: string = "4001";
+const mnemonic: string =
+"arch board rain drink turtle submit hidden service pond peasant ready dial buyer badge proof purchase thunder verb devote library pretty fat slot above resist";
+export function getClient(): algosdk.Algodv2 {
+let client = new algosdk.Algodv2(algodToken, server, port);
+return client;
+}
+export function getAccount(): algosdk.Account {
+let account = algosdk.mnemonicToSecretKey(mnemonic);
+return account;
+}
